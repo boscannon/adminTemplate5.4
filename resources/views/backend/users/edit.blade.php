fe531c5a4052b90d1d73b83be4b79ea8177a6f1f
boscannon <44830975+boscannon@users.noreply.github.com>
2023-08-09 00:03:54 +0800

    Update edit.blade.php

--- resources/views/backend/users/edit.blade.php
+++ resources/views/backend/users/edit.blade.php
@@ -6,16 +6,16 @@
             <form id="formData" action="{{ route('backend.'.$routeNameData.'.update',[$routeIdData => $data->id]) }}" method="post">
                 @csrf
                 @method('PUT')
-                <div class="form-row">
-                    <div class="form-group col-md-12 mb-4">
+                <div class="row">
+                    <div class="col-md-12 mb-4">
                         <label>{{ __("backend.$routeNameData.name") }}<span class="text-danger">*</span></label>
                         <input type="text" required name="name" class="form-control" value="{{ $data->name }}" placeholder="{{ __("backend.$routeNameData.name") }}">
                     </div>
-                    <div class="form-group col-md-12 mb-4">
+                    <div class="col-md-12 mb-4">
                         <label>{{ __("backend.$routeNameData.email") }}<span class="text-danger">*</span></label>
                         <input type="email" required name="email" class="form-control"  value="{{ $data->email }}" placeholder="{{ __("backend.$routeNameData.email") }}">
                     </div>
-                    <div class="form-group col-md-12 mb-4">
+                    <div class="col-md-12 mb-4">
                         <label>{{ __("backend.$routeNameData.roles") }}<span class="text-danger">*</span></label>
                         <select data-ajax--url="{{ route('backend.roles.select') }}" class="js-select2 form-control" required multiple name="roles[]" data-placeholder="{{ __("backend.$routeNameData.roles") }}">
                             @foreach($data->roles as $item)
@@ -24,15 +24,15 @@
                             <option></option>
                         </select>
                     </div>
-                    <div class="form-group col-md-12 mb-4">
+                    <div class="col-md-12 mb-4">
                         <label>{{ __("backend.$routeNameData.password") }}</label>
                         <input type="password" name="password" class="form-control"  value="" placeholder="{{ __("backend.$routeNameData.password") }}">
                     </div>
-                    <div class="form-group col-md-12 mb-4">
+                    <div class="col-md-12 mb-4">
                         <label>{{ __("backend.$routeNameData.password_confirmation") }}</label>
                         <input type="password" name="password_confirmation" class="form-control" value="" placeholder="{{ __("backend.$routeNameData.password_confirmation") }}">
                     </div>
-                    <div class="form-group col-md-6 mb-4">
+                    <div class="col-md-6 mb-4">
                         <label>{{ __("backend.$routeNameData.status") }}<span class="text-danger">*</span></label>
                         <div class="col-md-12">
                             <div class="form-check form-switch css-switch">
